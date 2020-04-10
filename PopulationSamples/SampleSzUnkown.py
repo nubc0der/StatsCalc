8c53bfa2ecdbe8b4e258e60f770a53b1398c3959
nubc0der <pkim437@gmail.com>
2020-04-10 17:03:49 -0400

    added population files

---
+++ PopulationSamples/SampleSzUnkown.py
@@ -0,0 +1,16 @@
+from StatisticFunctions.Zscore import Zscore
+from PopSamplingFunctions.MarginError import MarginError
+from MathOperations.Exponentiation import Exponentiation
+
+class SampleSizeUnkownPop():
+    @staticmethod
+
+    def sampleSize(sd, data, percentage):
+        z = Zscore.zscore(sd, data)
+        e = MarginError.marginError(sd, data)
+        p = percentage
+        q = 1 - p
+        val = z/e
+        sample = Exponentiation.exponent(val, 2) * p * q
+
+        return sample
