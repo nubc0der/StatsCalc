bf6a8cfc78fdb6310903c1fb48552a23fdedfdc7
nubc0der <pkim437@gmail.com>
2020-04-10 16:50:19 -0400

    added PopulationsSamples folder with population files

---
+++ PopulationSamples/ConfidenceIntervPop.py
@@ -0,0 +1,18 @@
+from scipy.stats import sem, t
+from StatisticTests.Mean import Mean
+
+
+class ConfidenceInterv():
+    @staticmethod
+
+    def confidenceInterv(conf, data):
+
+        lngth = len(data)
+        mean = Mean.mean(data)
+        std_err = sem(data)
+        high = std_err * t.ppf((1 + conf) / 2, lngth - 1)
+
+        start = mean - high
+        end = mean + high
+
+        return start, end
--- Statistics/Mean.py
+++ Statistics/Mean.py
@@ -1,14 +1,11 @@
-from MathOperations.addition import Addition
-from MathOperations.divison import Divsion
+from MathOperations.Addition import Addition
+from MathOperations.Division import Division
 
 class Mean:
-    def __init__(self):
-        pass
-
     @staticmethod
     def mean(data):
-    num_values = len(data)
-    total = 0
-    for num in data:
-        total = Addition(total, num)
-    return Divsion(num_values,total)
+        num_values = len(data)
+        total = 0
+        for num in data:
+            total = Addition.sum(total, num)
+        return Division.divide(total, num_values)
